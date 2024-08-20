e7b01a4f1f632d214a4bafbe720f259db57f2b81
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-08-20 00:42:30 -0300

    asd
    asd

--- main_3.py
+++ main_3.py
@@ -15,7 +15,7 @@ def handle_client(client_socket):
     client_socket.close()
 
 def main():
-    print("Logs from your programs will appear here!")
+    print("Laaogs from your programs will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     
